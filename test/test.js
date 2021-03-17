8edd69dbb2a32be607b90cfc293de5844a694056
asosa <asosa@legosoft.com.mx>
2021-03-16 18:54:00 -0600

    test erroneo

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const multiplicar = require('./../multiplicar');
 
 test('multiplicar 5 x 3 es igual a 15', () => {
-    expect(multiplicar(5, 3)).toBe(15);
+    expect(multiplicar(5, 4)).toBe(15);
 });
