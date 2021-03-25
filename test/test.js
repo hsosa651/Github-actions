1c519796ed40f933666a6c738ec13605631a9e02
asosa <asosa@legosoft.com.mx>
2021-03-25 12:14:33 -0600

    Se sube cambio

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const multiplicar = require('./../multiplicar');
 
 test('multiplicar 5 x 4 es igual a 15', () => {
-    expect(multiplicar(5, 3)).toBe(15);
+    expect(multiplicar(5, 3)).toBe(20);
 });
